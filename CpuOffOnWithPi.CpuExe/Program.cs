58578ce511ed8d88825e828ff11e64b55850eb64
Tyler Forsythe <forsythe.tyler@gmail.com>
2019-07-07 23:07:41 -0600

    Initial commit of projects, git ignore, and my README outlining the project and structure.

---
+++ CpuOffOnWithPi.CpuExe/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpuOffOnWithPi.CpuExe
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        }
+
+        /// <summary>
+        /// On Windows, this will require the user to be an admin!
+        /// </summary>
+        public static void ExecuteShutdown() {
+            Process.Start(new ProcessStartInfo("shutdown", "/s /t 0") {
+                CreateNoWindow = true, UseShellExecute = false
+            });
+        }
+    }
+}
